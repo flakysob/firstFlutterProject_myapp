ac53b161d41c7aa5df1d3e004a700523c55f162d
flakysob <flakysob@gmail.com>
2023-06-17 20:36:25 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+void main() =>  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Deneme"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Hello"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Click"),
+      ),
+      ),
+));
+
