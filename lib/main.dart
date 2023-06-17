b86c44235cd7b8e52392adc0fb9ad5cacf9171e4
flakysob <flakysob@gmail.com>
2023-06-18 02:18:06 +0300

    End of the Chapter 9

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,77 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
 
 void main() =>  runApp(MaterialApp(
-    home: Scaffold(
+    home: Home()
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Deneme"),
+        title: Text("myapp"),
         centerTitle: true,
+        backgroundColor: Colors.deepOrange,
       ),
       body: Center(
-        child: Text("Hello"),
+        child: IconButton(
+          onPressed: () {
+            print("Hello World!");
+          },
+          icon: Icon(Icons.alternate_email),
+          color: Colors.yellow,
+
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text("Click"),
+        backgroundColor: Colors.deepOrange,
       ),
-      ),
-));
+    );
+  }
+}
 
+
+
+
+
+
+
+
+
+
+// body: Center(
+// child: Icon(
+// Icons.access_time,
+// color: Colors.red,
+// size: 100.0,
+// shadows: [
+// BoxShadow(
+// color: Colors.black.withOpacity(0.5),
+// offset: Offset(4.0, 2.0),
+// blurRadius: 4.0,
+// ),
+// ],
+// ),
+// ),
+
+
+// ElevatedButton(
+// onPressed: () {
+// print('You clicked the ElevatedButton');
+// },
+// child: Text('Click the Button'),
+// style: ButtonStyle(
+// backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+// ),
+// ),
+
+//
+// TextButton.icon(
+// onPressed: () {},
+// icon: Icon(Icons.mail),
+// label: Text("Mail me"),
+// ),
+// ),
