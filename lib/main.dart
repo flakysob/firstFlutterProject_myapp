ac2c76741376808ce43a475219c4b1f2dcde2d53
flakysob <flakysob@gmail.com>
2023-06-18 19:50:47 +0300

    End of the Chapter 13 (Shortcuts & Outlines)

--- lib/main.dart
+++ lib/main.dart
@@ -14,57 +14,12 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.black38,
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              padding: EdgeInsets.all(20.0),
-              margin: EdgeInsets.all(10.0),
-              color: Colors.black12,
-              child: Center(child: ElevatedButton(child: Text("Click me"), onPressed: (){},)),
-            ),
-            Container(
-              padding: EdgeInsets.all(20.0),
-              margin: EdgeInsets.all(10.0),
-              color: Colors.black12,
-              child: Center(child: Text("This is my container", style: TextStyle(fontWeight: FontWeight.bold))),
-            ),
-            Container(
-              padding: EdgeInsets.all(20.0),
-              margin: EdgeInsets.all(10.0),
-              color: Colors.black12,
-              child: Center(child: Text("Hello world", style: TextStyle(fontWeight: FontWeight.bold))),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(0, 50.0, 0, 0),
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton(
-                onPressed: (){},
-                child: Text("Button 1"),
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.lime),
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton(
-                onPressed: (){},
-                child: Text("Button 2"),
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.orangeAccent),
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton(
-                onPressed: (){},
-                child: Text("Button 3"),
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.teal),
-              ),
-            ),
-          ],
-        ),
-
+      body: Row(
+        children: [
+          Container(),
+          Container(),
+          Container(),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
